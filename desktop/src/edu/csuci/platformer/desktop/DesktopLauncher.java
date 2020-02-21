7975b6a40980a09917185a528d4c4d9110c8b906
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-02-21 08:33:09 -0800

    Initial Commit

---
+++ desktop/src/edu/csuci/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.csuci.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.csuci.platformer.Platformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platformer(), config);
+	}
+}
